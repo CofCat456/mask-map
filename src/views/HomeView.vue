f56f9e9b73f56df553d2559204d213167e95e149
CofCat456 <a25690756@gmail.com>
2023-03-13 01:42:11 +0800

    chore: 🐙 install leaflet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,36 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import L from 'leaflet';
+
 import { apiCityData } from '../utlis/api';
 
+const map = ref({});
 const cityData = ref({});
 
-onMounted(async () => {
+const getCityData = async () => {
   const res = await apiCityData();
   cityData.value = res?.data?.features;
   console.log(cityData.value);
+};
+
+onMounted(() => {
+  getCityData();
+
+  map.value = L.map('map', {
+    center: [25.03, 121.55],
+    zoom: 14
+  });
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map.value);
 });
 </script>
 
 <template>
   <div class="relative space-x-0 flex flex-wrap">
-    <div class="w-1/4 bg-fuchsia-500">
+    <div class="w-1/4">
       <div class="toolbox">
         <div class="sticky bg-white shadow p-2">
           <div class="flex items-center mb-4">
@@ -43,7 +60,7 @@ onMounted(async () => {
         <ul class="flex flex-col"></ul>
       </div>
     </div>
-    <div class="flex-1 bg-zinc-600">
+    <div class="flex-1">
       <div id="map"></div>
     </div>
   </div>
